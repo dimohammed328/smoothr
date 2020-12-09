950403668360d44eaf8dd95098636b8c1051ef2e
Danish Mohammed <damo6513@colorado.edu>
2020-12-08 22:10:46 -0700

    adding file ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Loader from 'react-loader-spinner';
 import axios from 'axios';
 import './App.scss';
 
@@ -7,20 +7,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      files: [],
+      file: undefined,
     };
-    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
     this.fileUpload = this.fileUpload.bind(this);
-  }
-  onFormSubmit(e) {
-    e.preventDefault(); // Stop form submit
-    this.fileUpload(this.state.file).then(response => {
-      console.log(response.data);
-    });
+    this.check = this.check.bind(this);
   }
   onChange(e) {
-    Array.from(e.target.files).forEach(file => this.fileUpload(file))
+    this.fileUpload(e.target.files[0]);
+  }
+  check() {
+    console.log('checking');
+    const url =
+      'https://us-central1-graphical-envoy-287420.cloudfunctions.net/check';
+    axios
+      .get(url, { params: { file: this.state.file.outname } })
+      .then(res => {
+        console.log('res', res);
+        if (res.status === 200) {
+          this.setState({ url: res.data});
+          clearInterval(this.state.file.interval);
+        }
+        return '';
+      })
+      .catch(err => console.log('err', err));
   }
   fileUpload(file) {
     const url =
@@ -29,15 +39,48 @@ class App extends React.Component {
     formData.append('file', file);
     const config = {
       headers: {
-        'content-type': 'multipart/form-data'
+        'content-type': 'multipart/form-data',
       },
     };
-    axios.post(url, formData, config).then((res) => {
-      console.log(res)
-    }).catch(err => console.log(err))
+    axios
+      .post(url, formData, config)
+      .then(res => {
+        let f = {
+          name: file.name,
+          outname: res.data,
+          interval: setInterval(this.check, 3000),
+        };
+        this.setState({ file: f });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
+    let processing = '';
+    if (this.state.file) {
+      if (this.state.url) {
+        processing = (
+          <div className='file'>
+            <p className='filename'>{this.state.file.name}</p>
+            <a className='link' href={this.state.url}>
+              Download
+            </a>
+          </div>
+        );
+      } else {
+        processing = (
+          <div className='file'>
+            <p className='filename'>{this.state.file.name}</p>
+            <Loader
+              type='ThreeDots'
+              width={200}
+              height={200}
+              color='#FFD166'></Loader>
+          </div>
+        );
+      }
+    }
+
     return (
       <div>
         <div className='navbar'>
@@ -46,10 +89,11 @@ class App extends React.Component {
         <p>Upload MP4 to smooth the FPS!</p>
         <form>
           <label class='custom-file-upload'>
-            <input type='file' onChange={this.onChange}/>
+            <input type='file' onChange={this.onChange} />
             Upload
           </label>
         </form>
+        {processing}
       </div>
     );
   }
